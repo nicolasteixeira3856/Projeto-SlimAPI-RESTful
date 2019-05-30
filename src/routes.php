5b99059aec21bd63ce9e190aa435c3ef50f384b1
nicolasteixeira3856 <nicolasteixeira3856@outlook.com>
2019-05-29 21:42:23 -0300

    Arquivos inicias do projeto

---
+++ src/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+require __DIR__ . '/routes/produtos.php';
---
+++ src/routes/produtos.php
@@ -0,0 +1,11 @@
+<?php
+    use Slim\App;
+    use Slim\Http\Request;
+    use Slim\Http\Response;
+
+    //Routes
+    $app->group('/api/v1', function(){
+        $this->get('/produtos', function($request, $response){
+            return $response->withJson(['nome' => 'Moto G']);
+        });
+    });
